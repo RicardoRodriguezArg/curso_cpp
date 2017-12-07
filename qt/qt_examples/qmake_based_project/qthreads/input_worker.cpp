f876b7c846b4506d2bf911a99055097c2223a1ef
Alberto Ricardo Rodriguez <aricardorodriguez@hotmail.com>
2017-12-07 01:14:41 -0300

    [curso-qt] added custom threads

---
+++ qt/qt_examples/qmake_based_project/qthreads/input_worker.cpp
@@ -0,0 +1,6 @@
+#include "input_worker.h"
+
+QtCourse::input_worker::input_worker()
+{
+
+}
---
+++ qt/qt_examples/qmake_based_project/qthreads/input_worker.h
@@ -0,0 +1,11 @@
+#ifndef INPUT_WORKER_H
+#define INPUT_WORKER_H
+namespace QtCourse{
+
+class input_worker
+{
+public:
+    input_worker();
+};
+}
+#endif // INPUT_WORKER_H
